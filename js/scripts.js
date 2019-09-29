11b2a2963bd4069c19e19ee4c211f6aa8f331460
Willy Hardy <wjhrdy@gmail.com>
2019-09-29 14:39:59 -0400

    update basic info

--- js/scripts.js
+++ js/scripts.js
@@ -237,7 +237,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var itc_kol = {lat: 22.5932759, lng: 88.27027720000001};
+    var itc_kol = {lat: 35.7603985, lng: -78.6250251};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: itc_kol,
@@ -251,7 +251,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
+    var la_fiesta = {lat: 35.7603985, lng: -78.6250251};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,
